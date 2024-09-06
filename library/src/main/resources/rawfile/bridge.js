820457f26cac4206de3692450276413c103bbf39
nian1 <nian1@duck.com>
2024-09-06 21:59:43 +0800

    Update bridge

--- library/src/main/resources/rawfile/bridge.js
+++ library/src/main/resources/rawfile/bridge.js
@@ -3,17 +3,19 @@
     return;
   }
 
-  var receiveMessageQueue = [];
-  var messageHandlers = {};
-  var sendMessageQueue = [];
-  var responseCallbacks = {};
-  var uniqueId = 1;
-  var lastCallTime = 0;
-  var stoId = null;
-  var FETCH_QUEUE_INTERVAL = 20;
-  var messagingIframe;
-  var CUSTOM_PROTOCOL_SCHEME = "yy";
-  var QUEUE_HAS_MESSAGE = "__QUEUE_MESSAGE__";
+  let receiveMessageQueue = [];
+  const messageHandlers = {};
+  let sendMessageQueue = [];
+  const responseCallbacks = {};
+  let uniqueId = 1;
+  let lastCallTime = 0;
+  let stoId = null;
+  let messagingIframe;
+  let bizMessagingIframe;
+
+  const FETCH_QUEUE_INTERVAL = 20;
+  const CUSTOM_PROTOCOL_SCHEME = "yy";
+  const QUEUE_HAS_MESSAGE = "__QUEUE_MESSAGE__";
 
   function _createQueueReadyIframe() {
     messagingIframe = document.createElement("iframe");
@@ -35,9 +37,9 @@
     _createQueueReadyIframe();
     _createQueueReadyIframe4biz();
     WebViewJavascriptBridge._messageHandler = messageHandler;
-    var receivedMessages = receiveMessageQueue;
+    const receivedMessages = receiveMessageQueue;
     receiveMessageQueue = null;
-    for (var i = 0; i < receivedMessages.length; i++) {
+    for (let i = 0; i < receivedMessages.length; i++) {
       _dispatchMessageFromNative(receivedMessages[i]);
     }
     WebViewJavascriptBridge.inited = true;
@@ -64,31 +66,31 @@
   }
 
   function _doSend(handlerName, message, responseCallback) {
-    var callbackId;
-    if (typeof responseCallback === "string") {
-      callbackId = responseCallback;
-    } else if (responseCallback) {
-      callbackId = "cb_" + uniqueId++ + "_" + new Date().getTime();
-      responseCallbacks[callbackId] = responseCallback;
-      message.callbackId = callbackId;
-    } else {
-      callbackId = "";
+    let callbackId = "";
+    if (responseCallback) {
+      callbackId =
+        typeof responseCallback === "string" ? responseCallback : `cb_${uniqueId++}_${Date.now()}`;
+
+      if (typeof responseCallback === "function") {
+        responseCallbacks[callbackId] = responseCallback;
+      }
     }
+
+    if (callbackId) {
+      message.callbackId = callbackId;
+    }
+
     try {
-      var fn = eval("WebViewJavascriptBridge." + handlerName);
+      const fn = eval("WebViewJavascriptBridge." + handlerName);
+      if (typeof fn === "function") {
+        const responseData = fn.call(WebViewJavascriptBridge, JSON.stringify(message), callbackId);
+        if (responseData && responseCallbacks[callbackId]) {
+          responseCallbacks[callbackId](responseData);
+          delete responseCallbacks[callbackId];
+        }
+      }
     } catch (e) {
       console.log(e);
-    }
-    if (typeof fn === "function") {
-      var responseData = fn.call(WebViewJavascriptBridge, JSON.stringify(message), callbackId);
-      if (responseData) {
-        responseCallback = responseCallbacks[callbackId];
-        if (!responseCallback) {
-          return;
-        }
-        responseCallback(responseData);
-        delete responseCallbacks[callbackId];
-      }
     }
     sendMessageQueue.push(message);
     messagingIframe.src = CUSTOM_PROTOCOL_SCHEME + "://" + QUEUE_HAS_MESSAGE;
@@ -98,15 +100,16 @@
     if (sendMessageQueue.length === 0) {
       return;
     }
-    if (new Date().getTime() - lastCallTime < FETCH_QUEUE_INTERVAL) {
+    const currentTime = Date.now();
+    if (currentTime - lastCallTime < FETCH_QUEUE_INTERVAL) {
       if (!stoId) {
         stoId = setTimeout(_fetchQueue, FETCH_QUEUE_INTERVAL);
       }
       return;
     }
-    lastCallTime = new Date().getTime();
+    lastCallTime = currentTime;
     stoId = null;
-    var messageQueueString = JSON.stringify(sendMessageQueue);
+    const messageQueueString = JSON.stringify(sendMessageQueue);
     sendMessageQueue = [];
     bizMessagingIframe.src =
       CUSTOM_PROTOCOL_SCHEME + "://return/_fetchQueue/" + encodeURIComponent(messageQueueString);
@@ -114,8 +117,8 @@
 
   function _dispatchMessageFromNative(messageJSON) {
     setTimeout(function () {
-      var message = JSON.parse(messageJSON);
-      var responseCallback;
+      const message = JSON.parse(messageJSON);
+      let responseCallback;
       if (message.responseId) {
         responseCallback = responseCallbacks[message.responseId];
         if (!responseCallback) {
@@ -125,12 +128,12 @@
         delete responseCallbacks[message.responseId];
       } else {
         if (message.callbackId) {
-          var callbackResponseId = message.callbackId;
+          const callbackResponseId = message.callbackId;
           responseCallback = function (responseData) {
             _doSend("response", responseData, callbackResponseId);
           };
         }
-        var handler = WebViewJavascriptBridge._messageHandler;
+        let handler = WebViewJavascriptBridge._messageHandler;
         if (message.handlerName) {
           handler = messageHandlers[message.handlerName];
         }
@@ -152,8 +155,9 @@
   function _handleMessageFromNative(messageJSON) {
     if (receiveMessageQueue) {
       receiveMessageQueue.push(messageJSON);
+    } else {
+      _dispatchMessageFromNative(messageJSON);
     }
-    _dispatchMessageFromNative(messageJSON);
   }
 
   WebViewJavascriptBridge.init = init;
@@ -163,8 +167,8 @@
   WebViewJavascriptBridge._handleMessageFromNative = _handleMessageFromNative;
   WebViewJavascriptBridge._fetchQueue = _fetchQueue;
 
-  var readyEvent = document.createEvent("Events");
-  var jobs = window.WVJBCallbacks || [];
+  const readyEvent = document.createEvent("Events");
+  const jobs = window.WVJBCallbacks || [];
 
   readyEvent.initEvent("WebViewJavascriptBridgeReady");
   readyEvent.bridge = WebViewJavascriptBridge;
