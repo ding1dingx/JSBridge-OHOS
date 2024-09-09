1e32f7354f63f2287384bec98ac7a2174f51e4ac
nian1 <nian1@duck.com>
2024-09-09 19:31:23 +0800

    Optimizing the code

--- library/src/main/ets/entity/JSBridgeHelper.ts
+++ library/src/main/ets/entity/JSBridgeHelper.ts
@@ -1,25 +1,36 @@
-import { webview } from '@kit.ArkWeb';
-import { HashMap, JSON } from '@kit.ArkTS';
-import { JSRequest, JSResponse } from './JSTypes';
+import { HashMap } from '@kit.ArkTS';
 import { createLogger } from '../utils/Logger';
+import webview from '@ohos.web.webview';
 import hilog from '@ohos.hilog';
 
-const log = createLogger(0x0000, 'JSBridgeHelper');
+const log = createLogger(0x0000, "JSBridgeHelper");
 log.setLogLevel(hilog.LogLevel.INFO);
 
+interface JSRequest {
+  callbackId: string;
+  data?: string | object | null;
+  handlerName: string;
+}
+
+interface JSResponse {
+  responseId: string;
+  responseData?: string | object | null;
+}
+
 export class JSBridgeHelper {
-  controller: webview.WebviewController;
+  private controller: webview.WebviewController;
   messageMap: HashMap<string, (data: string) => void> = new HashMap();
 
   constructor(controller: webview.WebviewController) {
     this.controller = controller;
   }
 
-  sendResponse(data: string | object | null, callbackId: string) {
-    let message: JSResponse = new JSResponse();
-    message.responseId = callbackId;
-    message.responseData = data;
-    let content = `javascript:WebViewJavascriptBridge._handleMessageFromNative('${JSON.stringify(message)}');`;
+  sendResponse(data: string | object | null, callbackId: string): void {
+    const message: JSResponse = {
+      responseId: callbackId,
+      responseData: data,
+    };
+    const content = `javascript:WebViewJavascriptBridge._handleMessageFromNative('${JSON.stringify(message)}');`;
     log.info(`handleMessageFromNative ==> ${content}`);
     this.controller.runJavaScript(content);
   }
@@ -28,20 +39,21 @@ export class JSBridgeHelper {
     handlerName: string,
     data: string,
     callback?: (event: string) => void
-  ) {
-    let callbackId = `arkts_cb_${new Date().getTime()}`;
-    let message = new JSRequest();
-    message.data = data;
+  ): void {
+    const callbackId = `arkts_cb_${Date.now()}`;
+    const message: JSRequest = {
+      data,
+      handlerName,
+      callbackId: callback ? callbackId : "",
+    };
     if (callback) {
       this.messageMap.set(callbackId, callback);
-      message.callbackId = callbackId;
     }
-    message.handlerName = handlerName;
-    let content = `javascript:WebViewJavascriptBridge._handleMessageFromNative('${JSON.stringify(message)}');`;
+    const content = `javascript:WebViewJavascriptBridge._handleMessageFromNative('${JSON.stringify(message)}');`;
     this.controller.runJavaScript(content);
   }
 
-  sendToWeb(data: string) {
+  sendToWeb(data: string): void {
     this.callHandler("", `${data}-${Date.now()}`);
   }
 }
